e4daa1d533ab13b535e4c7c86255da0102c53cb8
Sanitha S <sanithasudevan@gmail.com>
2024-11-11 00:18:27 +0530

    featured tours

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -3,6 +3,7 @@ import Header from "../Header/Header";
 import Section1 from "../Sections/Section1";
 import Section2 from "../Sections/Section2";
 import Section3 from "../Sections/Section3";
+import FeaturedTours from "../Sections/FeaturedTours";
 
 
 const Layout = () =>
@@ -13,6 +14,7 @@ const Layout = () =>
             <Section1/>
             <Section2/>
             <Section3/>
+            <FeaturedTours/>
         </>
     );
 };
---
+++ src/components/Sections/FeaturedTours.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../styles/Featured_Tour.css';
+
+const FeaturedTours = () =>
+{
+  return (
+    <>
+      <div className="container">
+    <div className="row">
+        <div>
+            <h6 className="fontFamily p-2 rounded-pill bg-warning explore">
+                Explore
+            </h6><h2>Our featured tours</h2>
+        </div>
+    </div>
+</div>
+
+    </>
+  )
+}
+
+export default FeaturedTours
