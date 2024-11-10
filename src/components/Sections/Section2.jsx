6171811d8ebae08eb55672a29ab4664434b18eb5
Sanitha S <sanithasudevan@gmail.com>
2024-11-10 09:38:40 +0530

    search

--- src/components/Sections/Section2.jsx
+++ src/components/Sections/Section2.jsx
@@ -3,95 +3,60 @@ import '../../styles/Section2.css';
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
-
-
+import PinDropOutlinedIcon from '@mui/icons-material/PinDropOutlined';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
 
 const Section2 = () => {
     return (
-        <>
-            <div
-                className='bg light'
-                style={{
-                    marginLeft: '150px',
-                    maxWidth: '1000px',
-                }}
-            >
-                <div className="container mt-4 d-flex justify-content-start">
-                    <div
-                        className="row rounded-pill border p-3"
-                        style={{
-                            width: '100%',
-                            maxWidth: '750px',
-                            height: '80px',
-                            boxShadow: '0px 4px 30px rgba(0, 0, 0, 0.2)',
-                            borderColor: 'white',
-                            display: 'flex',
-                            justifyContent: 'flex-start',
-                            alignItems: 'center',
-                        }}
-                    >
-                        {/* Partition 1 */}
-                        <div
-                            className="col d-flex  justify-content-center border-end"
-                            style={{ height: '100%' }}
-                        >
-                            <div>
-                                <p>
-
-                                <PlaceOutlinedIcon style={{ fontSize: 15,color:'red', marginLeft:'-55px',marginTop:'-30px'}} />
-      
-                                </p>
-                                <p className="mb-0">Partition 1 - Row 2</p>
+        <div className="section2-container">
+            <div className="container mt-4 d-flex justify-content-start">
+                <div className="row rounded-pill border p-3 section2-form-wrapper">
+                    <form className="d-flex w-100">
+                        <div className="col d-flex flex-column section2-partition section2-border-end">
+                            <div className="d-flex align-items-center gap-2">
+                                <PlaceOutlinedIcon className="section2-header-icon" />
+                                <h6 className="section2-header-text">Location</h6>
                             </div>
+                            <input
+                                type="text"
+                                placeholder="Where are you going?"
+                                className="section2-input section2-input-location"
+                            />
                         </div>
 
-                        {/* Partition 2 */}
-                        <div
-                            className="col d-flex align-items-center justify-content-center border-end"
-                            style={{ height: '100%' }}
-                        >
-                            <div className="text-center">
-                                <p className="mb-0">Partition 2 - Row 1</p>
-                                <p className="mb-0">Partition 2 - Row 2</p>
+                        <div className="col d-flex flex-column section2-partition section2-border-end">
+                            <div className="d-flex gap-2">
+                                <PinDropOutlinedIcon className="section2-header-icon" />
+                                <h6 className="section2-header-text">Distance</h6>
                             </div>
+                            <input
+                                type="number"
+                                placeholder="Distance k/m"
+                                className="section2-input section2-input-distance"
+                            />
                         </div>
 
-                        {/* Partition 3 */}
-                        <div
-                            className="col d-flex align-items-center justify-content-center"
-                            style={{ height: '100%' }}
-                        >
-                            <div className="text-center">
-                                <p className="mb-0">Partition 3 - Row 1</p>
-                                <p className="mb-0">Partition 3 - Row 2</p>
+                        <div className="col d-flex flex-column section2-partition">
+                            <div className="d-flex gap-2">
+                                <GroupOutlinedIcon className="section2-header-icon" />
+                                <h6 className="section2-header-text">Max People</h6>
                             </div>
+                            <input
+                                type="number"
+                                placeholder="0"
+                                className="section2-input section2-input-people"
+                            />
                         </div>
 
-                        {/* Search Icon Button */}
-                        <div
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'flex-end',
-                                paddingLeft: '10px',
-                                marginTop: '-40px'
-                            }}
-                        >
-                            <IconButton
-                                style={{
-                                    backgroundColor: '#e76f0d',
-                                    borderRadius: '10%',
-                                    padding: '10px',
-                                    height: '35px',
-                                    width: '35px',
-                                }}
-                            >
-                                <SearchIcon style={{ color: 'white' }} />
+                        <div className="section2-search-button">
+                            <IconButton className="section2-icon-button">
+                                <SearchIcon className="section2-search-icon" />
                             </IconButton>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
